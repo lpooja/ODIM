cc2566b03f9d7ea91e5f823e7a4c98898c8f2842
lpooja <pooja.lakkundi@hpe.com>
2022-06-03 20:44:39 +0530

    replacing ip with domain name

--- svc-events/events/evtsubscription_test.go
+++ svc-events/events/evtsubscription_test.go
@@ -347,7 +347,7 @@ func TestFabricEventSubscription(t *testing.T) {
 	sessionUserName := "admin"
 	SubscriptionReq := map[string]interface{}{
 		"Name":                 "EventSubscription",
-		"Destination":          "https://localhost:8070/Destination1",
+		"Destination":          "https://odim.com:8070/Destination1",
 		"EventTypes":           []string{"Alert"},
 		"Protocol":             "Redfish",
 		"Context":              "Event Subscription",
