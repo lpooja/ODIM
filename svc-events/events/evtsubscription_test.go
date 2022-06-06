a143f2a1e577ec2bb0f63dbdecff29a95a5a184e
lpooja <pooja.lakkundi@hpe.com>
2022-06-06 11:08:50 +0530

    adding odim.com

--- svc-events/events/evtsubscription_test.go
+++ svc-events/events/evtsubscription_test.go
@@ -347,7 +347,7 @@ func TestFabricEventSubscription(t *testing.T) {
 	sessionUserName := "admin"
 	SubscriptionReq := map[string]interface{}{
 		"Name":                 "EventSubscription",
-		"Destination":          "https://odim.com:8070/Destination1",
+		"Destination":          "https://100.100.100.100:8070/Destination1",
 		"EventTypes":           []string{"Alert"},
 		"Protocol":             "Redfish",
 		"Context":              "Event Subscription",
