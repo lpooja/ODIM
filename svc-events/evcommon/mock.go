7d8f2b8c826d7f684e23600e9afd3b1b122c0c78
lpooja <pooja.lakkundi@hpe.com>
2022-06-06 17:20:21 +0530

    adding test cases

--- svc-events/evcommon/mock.go
+++ svc-events/evcommon/mock.go
@@ -263,7 +263,7 @@ func MockGetPluginData(pluginID string) (*evmodel.Plugin, *errors.Error) {
 		}
 	case "CFM":
 		plugin = &evmodel.Plugin{
-			IP:                "10.10.1.6",
+			IP:                "odim.cfm.com",
 			Port:              "4321",
 			Password:          password,
 			Username:          "admin",
