95dc2ff8498f9b442650b06211c44e9634d5e2d7
lpooja <pooja.lakkundi@hpe.com>
2022-06-06 12:53:23 +0530

    adding port to localhost

--- svc-events/events/evtsubscription_test.go
+++ svc-events/events/evtsubscription_test.go
@@ -347,7 +347,7 @@ func TestFabricEventSubscription(t *testing.T) {
 	sessionUserName := "admin"
 	SubscriptionReq := map[string]interface{}{
 		"Name":                 "EventSubscription",
-		"Destination":          "https://100.100.100.100:8070/Destination1",
+		"Destination":          "https://localhost:8070/Destination1",
 		"EventTypes":           []string{"Alert"},
 		"Protocol":             "Redfish",
 		"Context":              "Event Subscription",
