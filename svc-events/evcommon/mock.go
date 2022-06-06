e15fea9d6d6e57034e4148485e5d821e83ee2aa2
lpooja <pooja.lakkundi@hpe.com>
2022-06-06 12:23:03 +0530

    adding port to localhost

--- svc-events/evcommon/mock.go
+++ svc-events/evcommon/mock.go
@@ -427,7 +427,7 @@ func MockGetEvtSubscriptions(searchKey string) ([]evmodel.Subscription, error) {
 				MessageIds:           []string{},
 				ResourceTypes:        []string{},
 				OriginResources:      []string{"/redfish/v1/Fabrics/123456"},
-				Hosts:                []string{"10.10.0.1"},
+				Hosts:                []string{"localhost"},
 				SubordinateResources: true,
 			},
 		}
